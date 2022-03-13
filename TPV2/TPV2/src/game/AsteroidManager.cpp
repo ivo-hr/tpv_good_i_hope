00d3ff74bb2443ab1dbe6232318c13b4a73e5dd2
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2022-03-13 23:05:44 +0100

    final comments on asteroidmanager

--- TPV2/TPV2/src/game/AsteroidManager.cpp
+++ TPV2/TPV2/src/game/AsteroidManager.cpp
@@ -13,45 +13,55 @@
 #include "../components/Follow.h"
 #include "../components/Generations.h"
 
+//Method  initialization  and constructor
 AsteroidManager::AsteroidManager(ecs::Manager* mngr) :
 	asterNum_(0), 
-	score_(0), 
 	asterLimit_(30), 
 	counter_(1000.f),
 	mngr_(mngr)  {
 	lastSpawn_ = sdlutils().currRealTime();
 }
 
+//Destructor
 AsteroidManager::~AsteroidManager() {
 }
 
 
-
+//Asteroid deletion on collision
 void AsteroidManager::onCollision(ecs::Entity* a)
 {
+	//Kill asteroid
 	a->setAlive(false);
+
+	//Less asteroids in-game
 	asterNum_--;
+
+	//Next asteroid generation spawn
 	if (a->getComponent<Generations>()->getGen() > 0)
 	{
 		createAsteroids(2, a);
 	}
 }
 
-
+//Initial asteroids creation
 void AsteroidManager::createAsteroids(unsigned int n) {
 
+	//If not yet max  asteroids
 	if (asterNum_ < asterLimit_)
 	{
-
+		//Create asteroids until maxed out
 		for (auto i = 0u; i < n; i++) {
 
-			auto& rand = sdlutils().rand();
+			auto& rand = sdlutils().rand();			//Random number generator
 
+			//Entity creation and integration to group
 			auto e = mngr_->addEntity();
 			e->addToGroup(ecs::_grp_ASTEROIDS);
 
-			auto tr = e->addComponent<Transform>();
-			auto s = rand.nextInt(0, 3);
+			auto tr = e->addComponent<Transform>();	//Current asteroid Transform
+
+			//Size and generation
+			auto s = rand.nextInt(0, 3);			
 
 			e->addComponent<Generations>(s);
 
@@ -59,13 +69,15 @@ void AsteroidManager::createAsteroids(unsigned int n) {
 			else if (s == 1)	s = 40;
 			else				s = 80;
 
+			//Starting position
 			auto x = rand.nextInt(-s, sdlutils().width() + s);
 			auto y = rand.nextInt(-s, sdlutils().height() + s);
 
+			//Direction point
 			auto cx = rand.nextInt(sdlutils().width() / 2 - 100, sdlutils().width() / 2 + 100);
 			auto cy = rand.nextInt(sdlutils().height() / 2 - 100, sdlutils().height() / 2 + 100);
 
-
+			//Translation of starting position outside view, randomized  axis priority
 			if (rand.nextInt(0, 2) == 0) {
 
 				if (y < sdlutils().height() && y > 0)
@@ -105,63 +117,76 @@ void AsteroidManager::createAsteroids(unsigned int n) {
 
 			}
 
+			
+			tr->init(Vector2D(x, y), Vector2D(), s, s, 0.0f);	//Initialization of the asteroid
 
-			tr->init(Vector2D(x, y), Vector2D(), s, s, 0.0f);
 
+			auto sp = rand.nextInt(1, 5) / 10.0f;				//Speed of  the asteroid
 
-			auto sp = rand.nextInt(1, 5) / 10.0f;
+			//Asteroid type maker
 
+			//Asteroid that follows
 			if (rand.nextInt(0, 10) < 3)
 			{
-				e->addComponent<Follow>(mngr_->getHandler(ecs::_hdlr_FIGHTER)->getComponent<Transform>());
-				e->addComponent<FramedImage>(&sdlutils().images().at("asteroid_g"));
+				e->addComponent<Follow>(mngr_->getHandler(ecs::_hdlr_FIGHTER)->getComponent<Transform>());	//Following  component
+				e->addComponent<FramedImage>(&sdlutils().images().at("asteroid_g"));						//Image
 			}
+
+			//Normal asteroid
 			else
 			{
-				e->addComponent<FramedImage>(&sdlutils().images().at("asteroid"));
+				e->addComponent<FramedImage>(&sdlutils().images().at("asteroid"));							//Image
 
+				//Asteroid direction
 				auto dirX = ((cx - x) / 100) * sp;
 				auto dirY = ((cy - y) / 100) * sp;
 
-				e->addComponent<AsteroidMotion>();
-				e->getComponent<AsteroidMotion>()->dirSet(dirX, dirY);
+				e->addComponent<AsteroidMotion>();															//Moving component
+				e->getComponent<AsteroidMotion>()->dirSet(dirX, dirY);										//Direction setter
 			}
 
-			e->addComponent<ShowAtOppositeSide>();
+			e->addComponent<ShowAtOppositeSide>();															//Toroidal movement component
 
-			asterNum_++;
+			asterNum_++;																					//Current asteroid increase
 		}
 	}
 }
 
+//Regular asteroid spawner
 void AsteroidManager::addAsteroidFrequently()
 {
+	//If time  passed and in appropiate game state
 	if (mngr_->GetState() == ecs::RUNNING && 
 		lastSpawn_ + counter_ < sdlutils().currRealTime())
 	{
 		createAsteroids(1);
+		//Timer reset
 		lastSpawn_ = sdlutils().currRealTime();
 	}
 }
 
+//Asteroid destroyer
 void AsteroidManager::destroyAllAsteroids()
 {
-	auto& asteroids = mngr_->getEntitiesByGroup(ecs::_grp_ASTEROIDS);
+	auto& asteroids = mngr_->getEntitiesByGroup(ecs::_grp_ASTEROIDS);		//Asteroid group
 
-	auto n = asteroids.size();
+	auto n = asteroids.size();												//Group size
 
+	//Kill every asteroid in group
 	for (auto k = 0u; k < n; k++)
 	{
 		asteroids[k]->setAlive(false);
 	}
-	asterNum_ = 0;
+	asterNum_ = 0;															//Reset current asteroids number
 }
 
+//generational asteroid creation
 void AsteroidManager::createAsteroids(unsigned int n, ecs::Entity* a)
 {
+	//If not reached max asteroids yet
 	if (asterNum_ < asterLimit_)
 	{
-
+		//Create asteroids until indicated	(everything same as initial asteroid creation)
 		for (auto i = 0u; i < n; i++) {
 
 			auto& rand = sdlutils().rand();
@@ -181,8 +206,8 @@ void AsteroidManager::createAsteroids(unsigned int n, ecs::Entity* a)
 			auto cx = rand.nextInt(0, sdlutils().width());
 			auto cy = rand.nextInt(0, sdlutils().height());
 
-
-			tr->init(a->getComponent<Transform>()->getPos(), Vector2D(), s, s, 0.0f);
+			
+				tr->init(a->getComponent<Transform>()->getPos(), Vector2D(), s, s, 0.0f);		//!CHANGE! Spawning position of old asteroid
 
 			auto sp = rand.nextInt(1, 5) / 10.0f;
 
--- TPV2/TPV2/src/game/AsteroidManager.h
+++ TPV2/TPV2/src/game/AsteroidManager.h
@@ -8,24 +8,23 @@ class AsteroidManager {
 public:
 
 
-		AsteroidManager(ecs::Manager* mngr);
-	virtual ~AsteroidManager();
+		AsteroidManager(ecs::Manager* mngr);	//Class constructor
+	virtual ~AsteroidManager();					//class destroyer
 
-	void onCollision(ecs::Entity* a);
-	void addAsteroidFrequently();
-	void destroyAllAsteroids();
-	void createAsteroids(unsigned int n);
+	void onCollision(ecs::Entity* a);			//Collision consecuences
+	void addAsteroidFrequently();				//Regular asteroid spawner
+	void destroyAllAsteroids();					//Asteroids destroyer
+	void createAsteroids(unsigned int n);		//Initial asteroids creator
 
 private:
-	void createAsteroids(unsigned int n, ecs::Entity* a);
+	void createAsteroids(unsigned int n, ecs::Entity* a);	//Division asteroid creator
 	
-	unsigned int asterNum_;
-	unsigned int score_;
-	unsigned int asterLimit_;
+	unsigned int asterNum_;		//Current asteroid number
+	unsigned int asterLimit_;	//Max number of  asteroids
 
-	float counter_;
-	float lastSpawn_;
+	float counter_;				//Time counter since last spawn
+	float lastSpawn_;			//Last spawn time
 
 protected:
-	ecs::Manager* mngr_;
+	ecs::Manager* mngr_;		//Pointer to  current game manager
 };
